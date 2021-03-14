3136888dba346c3722e10e4a6c8c68024157d57f
Dima <brozhkodima@mail.ru>
2021-03-14 19:25:38 +0300

    Переименовал функцию add в add444

--- Form3.cs
+++ Form3.cs
@@ -30,7 +30,7 @@ namespace interface_example_Grishenko
         }
 
         // add money decrease operation
-        public add()
+        public add444()
         {
             table.add(id, name, summ, category);
         }
