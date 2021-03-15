05f4c20e7a44cacf57bf4e1da01041e9900dd171
Dima <brozhkodima@mail.ru>
2021-03-15 18:14:23 +0300

    Добавлены докблоки

--- Form1.cs
+++ Form1.cs
@@ -11,15 +11,61 @@ using System.Windows.Forms;
 namespace interface_example_Grishenko
 {
 
+	/**
+	 * Пользователь
+	 *
+	 * Класс описывает информацию 
+	 * о пользователе приложения. 
+	 * 
+	 * @author      BrozhkoDima
+	 * @version     1.0.0
+	 * @copyright   GNU Public License 
+	 * @todo        Добавить больше информации о пользователе 
+	 */
 	public class user
 	{
+		/** 
+		 * Имя пользователя 
+		 * 
+		 * Представляет собой имя пользователя 
+		 * 
+		 * @var   string name 
+		 */
 		string name;
+		/** 
+		 * Уникальный идентификатор пользователя 
+		 * 
+		 * Идентификатор пользователя 
+		 * для работы в базе данных
+		 * 
+		 * @var   int id
+		 */
 		int id;
+		/** 
+		 * Все данные пользователя 
+		 * 
+		 * Список данных о пользователе,
+		 * например, номер банковской карты,
+		 * номер телефона, дата оформления карты
+		 * и т.д. 
+		 * 
+		 * @var    List<string> user_info
+		 */
 		List<string> user_info;
 
-		public rename(string new_name)
+
+		/** 
+		 * Работа со свойством {@link name} 
+		 * 
+		 * Задается новое имя пользователя 
+		 * 
+		 * @param   string   new_name  Новое имя пользователя 
+		 * @return  string   name      Возвращает новое имя 
+		 */
+		public string rename(string new_name)
 		{
 			name = new_name;
+			return name;
 		}
 
 	}
